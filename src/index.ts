75832be2ec90a81c2ccdde1906dcc48d63a5ad0d
Kris Reeves <kris@pressbuttonllc.com>
2023-12-03 16:18:41 +0000

    Extract message utility functions from lobby-server into a separate repo
    We need some of this functionality in the NT app, which is currently a
    plain JS app, and also in the lobby server, which is a Typescript app.
    
    Pulling these functions out lets us build for both targets and maintain
    consistent behavior in consumers of the dependencies.

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export { ProtoHax } from './protohax/protohax';
+
+export {
+  createArmrCoder,
+  createDeltaCoder,
+  encodeBitfield,
+  decodeBitfield,
+  encodeStable,
+  decodeStable
+} from './compact_move';
+
+export { maybePlayerMove, tagPlayerMove } from './protoutil';
+
+export { M } from './util';
+
+export { NT } from './gen/pbjs_pb';
---
+++ src/util.ts
@@ -0,0 +1,39 @@
+import { NT } from './gen/pbjs_pb';
+import { gameActions, lobbyActions } from './pbreflect';
+
+export interface ActionCreator<T> {
+  (data: Exclude<T, undefined | null>, encoded: true): Uint8Array;
+  (data: Exclude<T, undefined | null>, encoded: false): NT.Envelope;
+  (data: Exclude<T, undefined | null>): NT.Envelope;
+}
+
+export type GameActionCreators = {
+  [K in keyof NT.IGameAction]-?: ActionCreator<NT.IGameAction[K]>;
+};
+export type LobbyActionCreators = {
+  [K in keyof NT.ILobbyAction]-?: ActionCreator<NT.ILobbyAction[K]>;
+};
+
+/**
+ * Factory functions for each action type. Each function
+ * accepts an action payload and returns an `NT.Envelope` instance
+ *
+ * @example
+ * ```ts
+ * M.cChat({ message: 'hi there' })
+ * ```
+ */
+export const M: GameActionCreators & LobbyActionCreators = {} as any;
+
+for (const key of gameActions) {
+  M[key] = ((data, encoded) =>
+    encoded
+      ? NT.Envelope.encode({ gameAction: { [key]: data } }).finish()
+      : NT.Envelope.fromObject({ gameAction: { [key]: data } })) as ActionCreator<NT.IGameAction[typeof key]>;
+}
+for (const key of lobbyActions) {
+  M[key] = ((data, encoded) =>
+    encoded
+      ? NT.Envelope.encode({ lobbyAction: { [key]: data } }).finish()
+      : NT.Envelope.fromObject({ lobbyAction: { [key]: data } })) as ActionCreator<NT.ILobbyAction[typeof key]>;
+}
